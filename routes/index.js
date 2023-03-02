7234236a71211ffce37b8f408504630306b6a236
Dylan Owens <dylan.owens06@ed.cna.nl.ca>
2023-03-02 17:02:10 -0330

    Express skeleton for Messageboard

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there cruel World!",
+    user: "Pizza",
+    added: new Date()
+  },
+  {
+    text: "Issa me!",
+    user: "Mario",
+    added: new Date()
+  }
+];
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'MessageBoard' });
+});
+
+router.get('/new', function(req, res){
+  res.render('form', { title: 'New Message'});
+});
+
+router.post('/new', (req, res) => {
+  // Handle form submission here
+});
+
+module.exports = router;
