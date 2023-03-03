3250c2a00a4dc306f33e791557d8a93934f47c08
Dylan Owens <dylan.owens06@ed.cna.nl.ca>
2023-03-03 19:13:10 -0330

    Fixed messages display | Added Css. | Added route to /new

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MessageBoard', messages: messages });
 });
 
-// router.get('/new', function(req, res){
-//   res.render('form', { title: 'New Message'});
-// });
+// GET form page.
+router.get('/new', function(req, res,){
+  res.render('form', { title: 'Messages', });
+  
+});
 
-// router.post('/new', (req, res) => {
-//   // Handle form submission here
-// });
+router.post('/', (req, res) => {
+  res.redirect('/new')
+})
+
+router.post('/new', (req, res) => {
+  // Handle form submission here
+const messageText = req.body.user_messages
+const messageUser = req.body.user_name_field
+
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+  res.redirect('/')
+});
+
+
 
 module.exports = router;
